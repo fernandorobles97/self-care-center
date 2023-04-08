32347b67c470c13098478ab9a98b9afb87f2c9bd
Fernando Robles <fernandorobles97@yahoo.com>
2023-04-08 17:10:24 -0500

    Feat: Add function that deletes messages

--- main.js
+++ main.js
@@ -36,8 +36,21 @@ function removeHiddenClass(elements) {
   }
 
 function deleteMssg() {
-
+    if (affirmBox.checked || mantraBox.checked) {
+        if (mantraBox.checked) {
+            for (var i = 0; i < mantras.length; i++) {
+                if (mantras[i] === message) {
+                    mantras.splice(i, 1);
+                    alert('You won\'t see this mantra anymore.');
+                }
+            }
+        } else {
+            for (var i = 0; i < affirmations.length; i++) {
+                if (affirmations[i] === message) {
+                    affirmations.splice(i, 1);
+                    alert('You won\'t see this affirmation anymore.');
+                }
+            }      
+        }
+    }
 }
-
-// Add the ability to delete a messge (ie: when a message shows up, show a button that says “I don’t like this messge” (or something similar), and remove it from the list so that it will not show up any more. Make sure to alert the user in some way that the message has been removed.
-// NOTE: None of this needs to persist on page refresh, unless you also complete the local storage feature
